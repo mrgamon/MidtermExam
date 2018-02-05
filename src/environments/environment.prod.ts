4ce9c2eb1a7e7a5a4a557b14b4e7f65300cf556b
mrgamon <mrgamon@hotmail.com>
2018-02-05 22:06:28 +0800

    added firebase initilization

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,1 +1,11 @@
-
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyCtBfEa4B_XeEYGoaaMML6wr14iS6XgcdY",
+        authDomain: "midtermexam-b9935.firebaseapp.com",
+        databaseURL: "https://midtermexam-b9935.firebaseio.com",
+        projectId: "midtermexam-b9935",
+        storageBucket: "midtermexam-b9935.appspot.com",
+        messagingSenderId: "102771758323"
+    }
+};
